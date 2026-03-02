33a9eaf6f4de0357947eacd0bf9dbf5a3487039f
guicortei <33794365+guicortei@users.noreply.github.com>
2026-03-02 20:29:41 +0000

    ok

--- tests/unit/calculadora.test.js
+++ tests/unit/calculadora.test.js
@@ -1,9 +1,9 @@
-const { somar } = require("../../models/calculadora.js");
+const calculadora = require("../../models/calculadora.js");
 
 test("espero que 1 + 1 seja 2", () => {
-  expect(somar(1, 1)).toBe(2);
+  expect(calculadora.somar(1, 1)).toBe(2);
 });
 
 test("espero que 2 + 2 seja 4", () => {
-  expect(somar(2, 2)).toBe(4);
+  expect(calculadora.somar(2, 2)).toBe(4);
 });
